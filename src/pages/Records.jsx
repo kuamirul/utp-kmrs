ba2d77903c0c897cf9713211e5b6e4965124cfad
Ku Amirul <kuamirulkuamir@gmail.com>
2024-06-08 12:04:50 +0800

    Add records page and service

--- src/pages/Records.jsx
+++ src/pages/Records.jsx
@@ -6,7 +6,7 @@ import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
 
 // components
-import CaseCard from '../components/CaseCard'
+import AllRecords from '../components/AllRecords'
 
 const Records = () => {
   const { user } = useAuth();
@@ -16,7 +16,7 @@ const Records = () => {
     <div className="container-flex" style={{ fontSize: "24px" }}>
       <h3 className="pl-3">UTP Records</h3>
       {fetchError && (<p>{fetchError}</p>)}
-      <CaseCard  />  
+      <AllRecords  />  
     </div>
     
   );
